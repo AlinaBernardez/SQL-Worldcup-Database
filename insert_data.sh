e9cc32e7f7a9c2dc8b97da800475de349fa50f87
A.B <116650041+AlinaBernardez@users.noreply.github.com>
2023-08-01 17:28:44 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,45 +8,32 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-echo "$($PSQL "TRUNCATE teams, games")"
+echo $($PSQL "TRUNCATE TABLE teams, games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do 
-#FILL TEAMS TABLE
-  if [[ $WINNER != "winner" ]]
-    then
-    #check team_id
-    WINNER_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    #if no team_id
-      if [[ -z $TEAM_ID ]]
-      then
-        #insert team and print result
-        INSERT_DATA_WINNERS=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-        #get team_id
-        WINNER_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-      fi
-  fi
-  if [[ $OPPONENT != "opponent" ]]
-    then
-    #check team_id
-    OPPONENT_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    #if no team_id
-      if [[ -z $TEAM_ID ]]
-      then
-        #insert team and print result
-        INSERT_DATA_WINNERS=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-        #get team_id
-        OPPONENT_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-      fi
-  fi
-  #FILL GAMES TABLE
-  if [[ $YEAR != 'year' && $ROUND != 'round' && $WINNER_GOALS != 'winner_goals' && $OPPONENT_GOALS != 'opponent_goals' ]]
+do
+  # Skip first row
+  if [[ $YEAR != "year" ]]
   then
-  GAME_ID=$($PSQL "SELECT game_id FROM games WHERE game_id=$GAME_ID")
-    if [[ -z $GAME_ID ]]
+    # Retrieve team_id for winner and opponent
+    TEAM_ID_W=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    TEAM_ID_O=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    # Add winner team to teams table and update variable if it does not already exist
+    if [[ -z $TEAM_ID_W ]]
     then
-      INSERT_GAME_DATA=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_TEAM_ID, $OPPONENT_TEAM_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
-      GAME_ID=$($PSQL "SELECT game_id FROM games WHERE game_id=$GAME_ID")
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      TEAM_ID_W=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
+
+    # Add opponent team to teams table and update variable if it does not already exist
+    if [[ -z $TEAM_ID_O ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      TEAM_ID_O=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # Add game details to games table
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $TEAM_ID_W, $TEAM_ID_O, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
